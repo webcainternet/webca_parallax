18a38335237febed3f888f98dc00aa27e058b118
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2014-01-09 00:18:03 -0200

    dw

--- public/wp-config.php
+++ public/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'webca_homolog2');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'webca_homolog2');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '12qw12qw');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'sv03.webca.com.br');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
